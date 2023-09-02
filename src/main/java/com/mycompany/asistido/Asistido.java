28df40b71fa701c5309e98f8b1ca67a4c8c0f419
GabrielCharry03 <gabrielchapi76@gmail.com>
2023-09-01 19:25:20 -0500

    Primer proyecto

---
+++ src/main/java/com/mycompany/asistido/Asistido.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.asistido;
+
+/**
+ *
+ * @author gabri
+ */
+public class Asistido {
+
+    public static void main(String[] args) {
+        
+    }
+}
